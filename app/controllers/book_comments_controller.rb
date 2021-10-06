cb1726589f444558a56156a5b710b0b239598cad
sakuow <yusaku1335@gmail.com>
2021-10-06 07:36:11 +0000

    9 commit

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,24 +1,24 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = BookComment.new(book_params)
-    comment.user_id = current_user.id
-    comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.all
+    @books = Book.find(params[:book_id])
+    @comment = BookComment.new(book_params)
+    @comment.user_id = current_user.id
+    @comment.book_id = @books.id
+    @comment.save
   end
 
   def destroy
-     @book = Book.find(params[:book_id])
-     book_comment = current_user.book_comments.find_by(id: params[:id], book_id: @book.id)
-     book_comment.destroy
-     redirect_to book_path(@book.id)
+    @books = Book.all
+    @book = Book.find(params[:book_id])
+    book_comment = current_user.book_comments.find_by(id: params[:id], book_id: @book.id)
+    book_comment.destroy
   end
 
   private
 
   def book_params
-    params.require(:book_comment).permit(:comment)
+    params.require(:book_comment).permit(:comment, :user_id, :book_id)
   end
 end
